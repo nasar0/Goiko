83d1d3099e765b406fa6c20211d42ecbf4ac3298
nasrallah <nasaroanas6@gmail.com>
2024-10-27 21:32:26 +0100

    2024-10-27

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // let enlaces = document.querySelectorAll("a"); 
-
-    // enlaces.forEach(enlace => {
-    //     let aVisible = enlace.querySelector(".visible"); 
-    //     let aOculto = enlace.querySelector(".oculto"); 
-
-        
-    //     enlace.addEventListener("mouseenter", () => {
-    //         aVisible.classList.add("giro"); 
-            
-            
-    //         setTimeout(() => {
-    //             aVisible.style.display = "none";
-    //             aOculto.style.display = "block"; 
-    //             setTimeout(() => {
-    //                 aOculto.style.opacity = "1"; 
-    //             }, 0); 
-    //         }, 800); 
-    //     });
-
-   
-    //     enlace.addEventListener("mouseleave", () => {
-    //         aVisible.classList.remove("giro")
-    //         aOculto.style.opacity = "0"; 
-    //         aVisible.style.opacity = "1"; 
-    //         aVisible.style.display = "block"; 
-
-            
-    //         setTimeout(() => {
-                
-    //         }, 0); 
-    //     });
-
-
-    // });
+    
     let ham = document.querySelector(".ham");
     let menu = document.querySelector("#menu");
     let cierre=document.querySelector("#cierre")
     ham.addEventListener("click", () => {
-        menu.style.display="block"
+        menu.style.display="block";
+        document.body.style.overflow = "hidden";
     });
     cierre.addEventListener("click", () => {
         menu.style.display="none"
+        document.body.style.overflow = "auto"; 
     });
 
 });
