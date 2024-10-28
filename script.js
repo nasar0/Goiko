6ca2b91f1cacda3130a82e687a141b1761fea636
Your Name <you@example.com>
2024-10-28 14:26:27 +0100

    2024-10-28

--- script.js
+++ script.js
@@ -12,4 +12,48 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = "auto"; 
     });
 
+        let enlaces = document.querySelectorAll("a")
+        
+        enlaces.forEach(enlace => {
+            let oculto = document.querySelector(".oculto")
+            let visible = document.querySelector(".visible")
+            enlace.addEventListener("mouseover", () => {
+                visible.classList.add("animate__animated","animate__flipOutX")
+                
+                setTimeout(()=>{
+                   visible.style.display="none"
+                   setTimeout(()=>{
+                        oculto.style.display = "block";
+                        oculto.style.opacity=1;
+                    },300)
+                },300)
+                
+                
+            })
+
+
+
+            // enlace.addEventListener("mouseover", () => {
+            //     visible.classList.add("animate__animated","animate__flipOutX")
+                
+            //     setTimeout(()=>{
+            //         visible.style.display="none"
+            //         oculto.style.display = "block";
+            //         oculto.style.opacity=1;
+            //     },300)
+            //     visible.classList.remove("animate__animated","animate__flipInX")
+                
+            // })
+            // enlace.addEventListener("mouseleave", () => {
+            //     oculto.classList.add("animate__animated","animate__flipOutX")
+            //     oculto.style.display="none"
+            //     visible.style.display = "block";
+            //     visible.style.opacity=1;
+            //     // visible.classList.remove("animate__animated","animate__flipInX")
+
+                
+            // })
+        });
+    
 });
+ 
